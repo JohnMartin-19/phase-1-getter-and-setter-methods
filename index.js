ed1950aef87de4b0bc82b22d5c447a87de15f56e
John <johnnkonge2020@gmail.com>
2023-10-21 09:55:26 +0300

    setter and getters

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your Circle class here
+class circle{
+    #pi
+    constructor(radius){
+        this.radius = radius
+    }
+    set pi (pi){
+        this.#pi = pi
+    }
+    get area(){
+        console.log (this.#pi * this.radius * this.radius)
+    }
+}
